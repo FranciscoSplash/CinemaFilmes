bc88bd501828e1765b8c27bec1c117591f817bf6
FranciscoSplash <franciscomauricio796@gmail.com>
2026-05-27 21:51:04 -0300

    Refatorizando os modelos do projetos

--- src/main/java/com/Senai/Filmes/Model/Assento.java
+++ src/main/java/com/Senai/Filmes/Model/Assento.java
@@ -11,7 +11,7 @@ import org.hibernate.annotations.CreationTimestamp;
 import java.time.LocalDateTime;
 import java.util.UUID;
 
-@Data
+@Data  
 @NoArgsConstructor
 @Entity
 @Table(name = "acentos")
