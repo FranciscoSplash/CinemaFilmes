e7490a249a16cc3d7cfa4893acd1fd935d5741e8
Francisco <franciscomauricio796@gmail.com>
2026-06-14 21:33:56 -0300

    feat: reajustos das classes

--- src/main/java/com/Senai/Filmes/Repository/ISessaoRepository.java
+++ src/main/java/com/Senai/Filmes/Repository/ISessaoRepository.java
@@ -5,16 +5,17 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.UUID;
 
 public interface ISessaoRepository extends JpaRepository<Sessao, UUID> {
 
+    @Query("SELECT s FROM Sessao s WHERE s.inicio >= :inicioDia AND s.inicio < :fimDia")
+    List<Sessao> findByData(@Param("inicioDia") LocalDateTime inicioDia,
+                            @Param("fimDia") LocalDateTime fimDia);
 
-    @Query("SELECT ra.assento.id FROM ReservaAssento ra " + " WHERe iniio.id = :sessaoId And ra.reserva.status =:status")
-    List<Sessao> fingdByDate(@Param("inicioDia"));
+    @Query("SELECT s FROM Sessao s WHERE s.filme.id = :filmeId ORDER BY s.inicio ASC")
+    List<Sessao> findByFilmeId(@Param("filmeId")UUID filmeId);
 
-
-
-    List <Sessao> findBFilmefindBY();
 }
